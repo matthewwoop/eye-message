7019d10376b30806d7269200bfba99a193052d31
Matthew Woop <matthew.woop@gmail.com>
2019-04-21 10:52:31 -0400

    Adds sql query to obtain a messages via nested SELECTs

--- messages.sql
+++ messages.sql
@@ -12,3 +12,29 @@ FROM chat_message_join AS cmj
         ON cmj.chat_id = chj.chat_id
 ORDER BY chj.handle_id DESC
 LIMIT 1;
+
+-- grab a single text
+SELECT
+    text
+FROM
+    message
+WHERE
+    handle_id=(
+    SELECT
+        handle_id
+    FROM
+        chat_handle_join
+    WHERE
+        chat_id=(
+            SELECT
+                ROWID
+            FROM
+                chat
+            WHERE
+                guid=(
+                    SELECT DISTINCT guid
+                FROM chat
+                LIMIT 1
+            )
+        )
+    );
